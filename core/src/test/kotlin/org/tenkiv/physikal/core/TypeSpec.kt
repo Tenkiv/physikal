67a39c9ef2ae45a74a464eee9831eddb63da9d04
Zack Juhasz <zjuhasz@tenkiv.com>
2017-10-10 14:12:01 -0700

    Moved type tests to new class.

---
+++ core/src/test/kotlin/org/tenkiv/physikal/core/TypeSpec.kt
@@ -0,0 +1,19 @@
+package org.tenkiv.physikal.core
+
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.StringSpec
+import tec.uom.se.unit.Units
+import javax.measure.quantity.Power
+import javax.measure.quantity.Pressure
+
+class TypeSpec : StringSpec({
+    "type coercion on quantities" {
+        1.watt.asTypeOrNull<Pressure>() shouldBe null
+        1.watt.asTypeOrNull<Power>() shouldBe 1.watt
+    }
+
+    "type coercion on units" {
+        Units.WATT.asTypeOrNull<Pressure>() shouldBe null
+        Units.WATT.asTypeOrNull<Power>() shouldBe Units.WATT
+    }
+})
