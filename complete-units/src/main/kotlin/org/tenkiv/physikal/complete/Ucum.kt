d53b8102d686af8e3da52218aeccd126a826cd69
Zack Juhasz <zjuhasz@tenkiv.org>
2019-03-28 08:46:09 +0000

    Copyright update

--- complete-units/src/main/kotlin/org/tenkiv/physikal/complete/Ucum.kt
+++ complete-units/src/main/kotlin/org/tenkiv/physikal/complete/Ucum.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Tenkiv, Inc.
+ * Copyright 2019 Tenkiv, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
@@ -14,6 +14,7 @@
  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
  * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 package org.tenkiv.physikal.complete
 
 import org.tenkiv.physikal.core.MetricPrefixedNumber
